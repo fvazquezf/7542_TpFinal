cbfd76f3e0e751ccc2e68cd7899269bb7dad7e51
TheGrey <fvazquezf@fi.uba.ar>
2021-06-07 17:22:09 -0300

    agrego caja, el personaje choca. El movimiento sigue siendo entrecortado, hay que modificarlo para que registre que se mantiene apretado un boton. Las dimensiones estan hardcodeadas

--- client/demo_movimiento.cpp
+++ client/demo_movimiento.cpp
@@ -18,7 +18,7 @@ b2Body* createBody(b2World& world){
 
 	b2BodyDef bodyDef;
 	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 4.0f);
+	bodyDef.position.Set(0.0f, 0.0f);
 	b2Body* body = world.CreateBody(&bodyDef);
 
 	b2PolygonShape bodyBox;
@@ -44,23 +44,29 @@ b2Body* createBody(b2World& world){
 
 
 int main(int argc, char const *argv[]){
-	SdlWindow w(800, 600, false, "unaVentana");
+	SdlWindow w(600, 400, false, "unaVentana");
 	SdlTexture t("../sprites/gfx/player/ct1.bmp", w);
+    SdlTexture tCaja("../sprites/gfx/tiles/cs2dnorm.bmp", w);
 	// Explotion exp(t);
 
 	bool running = true;
-	
-	Area src(0, 0, 64, 64);
-	Area dest(200, 200, 64, 64);
-	w.fill();
-	t.render(src, dest, SDL_FLIP_NONE);
-	w.render();
+
 	SDL_Event e;
 
 	b2Vec2 gravity(0.0f, 0.0f);
 	b2World world(gravity);
 
     b2Body* body = createBody(world);
+
+    b2BodyDef boxDef;
+	boxDef.type = b2_staticBody;
+	boxDef.position.Set(0.0f, -50.0f);
+	b2Body* box = world.CreateBody(&boxDef);
+    b2PolygonShape bodyBox;
+	bodyBox.SetAsBox(15.0f, 15.0f);
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &bodyBox;
+    box->CreateFixture(&fixtureDef);
 
 	b2Vec2 f1(0, -100);
 
@@ -71,10 +77,13 @@ int main(int argc, char const *argv[]){
 	int32 velocityIterations = 6;
 	int32 positionIterations = 2;
 
+    Area boxSrc(0, 224, 32, 32);
+    Area boxDest(box->GetPosition().x + 300 - 16, box->GetPosition().y + 200 - 16, 16, 16);
+
 	while (running){
 		while (SDL_WaitEvent(&e)){
 			if (e.type == SDL_MOUSEBUTTONUP){
-				body->ApplyForceToCenter(f1, true);			 
+				body->ApplyLinearImpulseToCenter(f1, true);			 
 			}
 			if (e.type == SDL_QUIT){
 				running = false;
@@ -85,10 +94,11 @@ int main(int argc, char const *argv[]){
             float angle = body->GetAngle();
 
             Area src(0, 0, 32, 32);
-            Area dest(position.x + 150, position.y + 150, 64, 64);
+            Area dest(position.x + 300 - 16, position.y + 200 - 16, 16, 16);
 
             w.fill();
             t.render(src, dest, SDL_FLIP_NONE);
+            tCaja.render(boxSrc, boxDest, SDL_FLIP_NONE);
             w.render();
 
             printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);	
