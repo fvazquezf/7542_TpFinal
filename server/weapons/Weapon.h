eee42cbcf5a875b2ec7e37ea98758ee1da544be0
leogm99 <lgiampieri62@gmail.com>
2021-06-25 18:07:05 -0300

    Arreglo clase Weapon, faltaba hacer los metodos abstractos

--- server/weapons/Weapon.h
+++ server/weapons/Weapon.h
@@ -10,13 +10,13 @@ protected:
 public:
     Weapon();
 
-    virtual bool attack(const b2Vec2& player, int16_t angle, const b2Vec2& enemy);
+    virtual bool attack(const b2Vec2& player, int16_t angle, const b2Vec2& enemy) = 0;
     
-    virtual bool tickCooldown();
+    virtual bool tickCooldown() = 0;
 
-    virtual void resetCooldown();
+    virtual void resetCooldown() = 0;
 
-    virtual int hit();
+    virtual int hit() = 0;
 
     virtual ~Weapon();
 };
