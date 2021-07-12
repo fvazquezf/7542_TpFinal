fb03437eb3a85472aa6b6be21c71b0b50df8a2e9
Ana Maria Cai <acai@fi.uba.ar>
2021-07-12 13:47:59 -0300

    Agrego desinstaller

--- README.md
+++ README.md
@@ -8,11 +8,13 @@
 
 ## Tabla de contenidos
 
-1. [Introduccion](#Introduccion)
-2. [Manual de instalacion ](#Manual de instalacion)
-    1. [Instalacion](#Instalacion)
-    2. [Desinstalacion](#Desinstalacion)
-3. [Manual de usuario](#Manual de ususario)
+- [7542_TpFinal](#7542_tpfinal)
+  - [Tabla de contenidos](#tabla-de-contenidos)
+  - [Introduccion](#introduccion)
+  - [Manual de instalacion](#manual-de-instalacion)
+    - [Instalacion](#instalacion)
+    - [Desinstalacion](#desinstalacion)
+  - [Manual de ususario](#manual-de-ususario)
 
 ## Introduccion
 
@@ -21,11 +23,27 @@
 
 ### Instalacion
 
+Para instalar las dependencias del juego se debe correr el siguiente comando en la consola:
+
 ```console
-foo@bar:~$ sh installer.sh
+sh installer.sh
 ```
 
 ### Desinstalacion
 
+Para instalar las dependencias del juego se debe correr el siguiente comando en la consola:
+
+```console
+sh desinstaller.sh <flags>
+```
+
+Las flags que pueden haber y combinar son:
+
+- `--qt`:  borra las dependencias de qt
+- `--sdl`:  borra las dependencias de sdl2
+- `--yaml`:  borra las dependencias de yaml-cpp
+- `--cpp`:  borra las dependencias: clang, cmake, make y build-essential
+- `--repo`:  elimina el repositorio
+- `--all`:  borra las dependencias de qt, sdl2, yaml-cpp, clang, cmake, make y build-essentia, y elimina el repositorio
 
 ## Manual de ususario
---
+++ desinstaller.sh
@@ -0,0 +1,134 @@
+#!/usr/bash
+
+#!/usr/bash
+
+green=`tput setaf 2`
+bold=`tput bold`
+reset=`tput sgr0`
+
+echo "${green}${bold}\n********************************************************************************${reset}"
+echo "${green}${bold}*****************************DESINSTALADOR*****************************************${reset}"
+echo "${green}${bold}********************************************************************************${reset}"
+
+# Loop through arguments and process them
+for arg in "$@"
+do
+    case $arg in
+
+        --qt)
+        shift
+        echo "${green}${bold}Desinstalando las dependencias de qt...${reset}"
+
+        echo "${green}Desinstalando 'qt5'...${reset}"
+        sudo apt-get remove qt5-default -y
+
+        echo "${green}Desinstalando 'libqt5multimedia5-plugnins'...${reset}"
+        sudo apt-get remove libqt5multimedia5-plugins -y
+        
+        ;;
+
+
+
+        --yaml)
+        shift
+        echo "${green}${bold}Desinstalando las dependencias de yaml-cpp...${reset}"
+
+        sudo apt-get remove libyaml-cpp-dev -y
+
+        ;;
+
+
+
+        --sdl)
+        shift
+        echo "${green}${bold}Desinstalando las dependencias de sdl2...${reset}"
+
+        echo "${green}Desinstalando 'libsdl2-dev'...${reset}"
+        sudo apt-get remove libsdl2-dev -y
+
+        echo "${green}Desinstalando 'libsdl2-image-dev'...${reset}"
+        sudo apt-get remove libsdl2-image-dev -y
+
+        echo "${green}Desinstalando 'libsdl2-gfx-dev'...${reset}"
+        sudo apt-get remove libsdl2-gfx-dev -y
+
+        echo "${green}Desinstalando 'libsdl2-mixer-dev'...${reset}"
+        sudo apt-get remove libsdl2-mixer-dev -y
+        
+        ;;
+
+
+
+        --cpp)
+        shift
+        echo "${green}${bold}Desinstalando las dependencias cpp...${reset}"
+
+        echo "${green}Desinstalando 'clang'...${reset}"
+        sudo apt-get remove clang -y 
+
+        echo "${green}Desinstalando 'make'...${reset}"
+        sudo apt-get remove make -y 
+
+        echo "${green}Desinstalando 'cmake'...${reset}"
+        sudo apt-get remove cmake -y
+
+        echo "${green}Desinstalando 'build-essential'...${reset}"
+        sudo apt-get remove build-essential -y
+
+        ;;
+
+
+        --repo)
+        shift
+        echo "${green}${bold}Borrando el repositorio...${reset}"
+        cd ..
+        rm 7542_TpFinal
+        ;;
+
+
+
+        --all)
+        shift
+
+        echo "${green}Desinstalando 'qt5'...${reset}"
+        sudo apt-get remove qt5-default -y
+
+        echo "${green}Desinstalando 'libqt5multimedia5-plugnins'...${reset}"
+        sudo apt-get remove libqt5multimedia5-plugins -y
+        
+        echo "${green}Desinstalando las dependencias de yaml-cpp...${reset}"
+        sudo apt-get remove libyaml-cpp-dev -y
+
+        echo "${green}Desinstalando 'libsdl2-dev'...${reset}"
+        sudo apt-get remove libsdl2-dev -y
+
+        echo "${green}Desinstalando 'libsdl2-image-dev'...${reset}"
+        sudo apt-get remove libsdl2-image-dev -y
+
+        echo "${green}Desinstalando 'libsdl2-gfx-dev'...${reset}"
+        sudo apt-get remove libsdl2-gfx-dev -y
+
+        echo "${green}Desinstalando 'libsdl2-mixer-dev'...${reset}"
+        sudo apt-get remove libsdl2-mixer-dev -y
+        
+        echo "${green}Desinstalando 'clang'...${reset}"
+        sudo apt-get remove clang -y 
+
+        echo "${green}Desinstalando 'make'...${reset}"
+        sudo apt-get remove make -y 
+
+        echo "${green}Desinstalando 'cmake'...${reset}"
+        sudo apt-get remove cmake -y
+
+        echo "${green}Desinstalando 'build-essential'...${reset}"
+        sudo apt-get remove build-essential -y
+
+        cd ..
+        rm 7542_TpFinal
+
+        ;;
+
+
+
+    esac
+done
